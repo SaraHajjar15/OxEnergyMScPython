7e6b4bfd6321ad55f7ae99846fdd2f732b114c77
SaraHajjar15 <reub0751@ox.ac.uk>
2025-10-28 09:44:51 +0000

    Day 1 python, need to correct

--- NotebooksScripts/FunctionsPVStarter.py
+++ NotebooksScripts/FunctionsPVStarter.py
@@ -7,24 +7,36 @@ Learning objectives:
 
 Complete the script by filling in the missing code sections marked with <---.
 
-@author: PLACE YOUR NAME HERE
+@author: Sara Hajjar 
 """
 
 # Import any necessary libraries
 import math
-import pandas as pd
-import numpy as np
-import os
+# import pandas as pd
+# import numpy as np
+# import os
 
 # <--- Define a function to size a PV system based on building dimensions and panel specifications
-def calculate_pv_size(): # <--- include parameters for building length, width, roof angle, panel width, panel height and panel power
+def calculate_pv_size(building_length, building_width, roof_angle, panel_width, panel_height, panel_power): # <--- include parameters for building length, width, roof angle, panel width, panel height and panel power
     """
     This is a docstring. Use it to describe the function's purpose, parameters, and return values.
     """
+    """
+    building_length in meters = 30
+    building_width in meters = 16
+    roof_angle in degrees = 22
 
+    panel_width in mm = 1690
+    panel_height in mm = 1046
+    panel_power in Wp = 400
+    """
 
-
-    return # <--- return the total PV capacity in kW and number of panels
+    num_panels_length = (building_length) / (panel_width / 1000)  # <--- calculate number of panels that fit along the length of the building
+    num_panels_width = (building_width) / (panel_width / 1000)  # <--- calculate number of panels that fit along the width of the building
+    num_panels = math.floor(num_panels_length) * math.floor(num_panels_width)  # <--- calculate total number of panels (use math.floor to round down to nearest whole number)
+    pv_capacity_kw = (num_panels * panel_power) / 1000  #
+    
+    return pv_capacity_kw, num_panels # <--- return the total PV capacity in kW and number of panels
 
 if __name__ == "__main__":
     # =============================================================================
@@ -39,6 +51,6 @@ if __name__ == "__main__":
     # use the code.
     # =============================================================================
     
-    pv_capacity_kw, num_panels = # <--- call the calculate_pv_size function with appropriate arguments
+    pv_capacity_kw, num_panels = calculate_pv_size(30, 8, 22, 1690, 1046, 400)# <--- call the calculate_pv_size function with appropriate arguments
 
-    print() # <--- Add a print statement to display the number of PV panels and the total PV capacity in kW
+    print(pv_capacity_kw, num_panels) # <--- Add a print statement to display the number of PV panels and the total PV capacity in kW
